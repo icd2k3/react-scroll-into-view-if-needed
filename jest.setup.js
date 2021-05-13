b8b507545cdf4facc471af61b4ac0a31d6b3eccc
Francois Henderson <francois.henderson@qc.croixbleue.ca>
2021-05-13 19:41:44 -0400

    build(dependencies): Update dependencies, React and Node version target

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require('enzyme-adapter-react-17-updated');
 
 Enzyme.configure({ adapter: new Adapter() });
